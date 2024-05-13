85e627aba376eb2c8a9c7556df61a212342e84cd
macielcaio <caiohenriquemaciel@gmail.com>
2024-05-13 18:59:50 -0300

    New class and model of tarefas from challenge

--- src/list/OperacoesBasicas/ListaTarefa.java
+++ src/list/OperacoesBasicas/ListaTarefa.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class ListaTarefa {
     //atributo
-    private List<Tarefa> tarefaList;
+    private final List<Tarefa> tarefaList;
 
     public ListaTarefa() {
         this.tarefaList = new ArrayList<>();
